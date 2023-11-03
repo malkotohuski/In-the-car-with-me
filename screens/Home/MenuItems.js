cfddb699382b47d018defbb77f18dafdd8e2bfa8
Daniel Dimitrov <malkotohuski@gmail.com>
2023-11-03 15:22:42 +0200

    home skeleton

---
+++ screens/Home/MenuItems.js
@@ -0,0 +1,44 @@
+const menuItems = [
+    {
+      image: 'https://img.freepik.com/free-vector/realistic-complete-set-car-wheels_1284-29765.jpg?size=626&ext=jpg',
+      label: 'Tires',
+    },
+    {
+      image: 'https://s.aolcdn.com/commerce/blogcdn/www.autoblog.com/media/2006/09/advancedvtec3.jpg',
+      label: 'Engine',
+    },
+    {
+      image: 'https://upload.wikimedia.org/wikipedia/commons/2/21/Mercedes-Benz_7G-Tronic_transmission.jpg',
+      label: 'Gearbox',
+    },
+    {
+      image: 'https://di-uploads-pod16.dealerinspire.com/toyotaofnorthcharlotte/uploads/2020/03/N-Charlotte-car-suspension-system.jpg',
+      label: 'Suspension',
+    },
+    {
+      image: 'https://autotrends.org/wp-content/uploads/motor-oil-weights.jpg',
+      label: 'Motor Oil',
+    },
+    {
+      image: 'https://c8.alamy.com/comp/2BB3X6C/braking-system-car-brake-disks-with-different-perforations-and-calipers-isolated-on-white-background-3d-illustration-2BB3X6C.jpg',
+      label: 'Braking System',
+    },
+    {
+      image: 'https://carauto.bg/ckfinder/userfiles/images/3.jpg',
+      label: 'Ignition Systems',
+    },
+    {
+      image: 'https://www.avtotachki.com/wp-content/uploads/2021/01/konstrukcija-kuzova-avtomobilya.jpg',
+      label: 'Car body',
+    },
+    {
+      image: 'https://maslata.bg/wp-content/uploads/2015/03/1.jpg',
+      label: 'Filter',
+    },
+    {
+      image: 'https://i.gaw.to/content/photos/26/98/269855_La_petite_histoire_de_la_direction_assistee.jpg?1024x640',
+      label: 'Steering Suspension',
+    },
+  ];
+
+  export default menuItems
