c064867e0b41ecc17339231bb9ba6b7f77dd7e79
Daniel Dimitrov <malkotohuski@gmail.com>
2023-11-03 13:58:01 +0200

    login screen

---
+++ App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+} from 'react-native';
+
+export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // Add your login logic here, for example, checking the email and password.
+    if (email === 'user@example.com' && password === 'password') {
+      // Successful login, you can navigate to another screen or perform any action.
+      alert('Login successful');
+    } else {
+      // Display an error message for an unsuccessful login.
+      alert('Login failed. Please check your credentials.');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login Screen</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Button
+        title="Submit"
+        onPress={handleLogin}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
