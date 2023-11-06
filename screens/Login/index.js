cfcd77fc7e5e01b2dbfc6321d3f9043edfdd11ff
Daniel Dimitrov <malkotohuski@gmail.com>
2023-11-06 16:00:20 +0200

    greate stackNavigatore

---
+++ screens/Login/index.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+export default function Login() {
+    const navigation = useNavigation();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // Add your login logic here
+      if (email === 'user@example.com' && password === 'password') {
+        // Navigate to the HomeScreen upon successful login
+        navigation.navigate('Home');
+      } else {
+        alert('Login failed. Please check your credentials.');
+      }
+    };
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Login Screen</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Button title="Submit" onPress={handleLogin} />
+        <View style={styles.buttonSeparator} />
+        <Button title="Register" onPress={() => navigation.navigate('Register')} />
+      </View>
+    );
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: 16,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    input: {
+      width: '100%',
+      height: 40,
+      borderWidth: 1,
+      borderColor: 'gray',
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+    buttonSeparator: {
+      height: 10,
+    },
+  });
