d1f1b9b92e4762397cf35bc6ae12b48bc684e82f
Daniel Dimitrov <malkotohuski@gmail.com>
2023-11-16 12:28:41 +0200

    vector icons added

--- screens/DrawerContent/tabsNavigator.js
+++ screens/DrawerContent/tabsNavigator.js
@@ -1,48 +1,98 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomePage from '../Home/Home';
 import Login from '../Login';
 import Register from '../Register';
 import MyAccount from '../Account';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { useNavigation } from '@react-navigation/native';
+
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
+    const navigation = useNavigation();
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomePage}
+        <Tab.Navigator
+            screenOptions={{
+                showLabel: false,
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    height: 60,
+                },
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={HomePage}
                 options={{
-                    title: 'Home',
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
+                    tabBarIcon: ({ focused }) => (
+                        <TouchableOpacity
+                            style={{ alignItems: 'center', justifyContent: 'center' }}
+                            onPress={() => navigation.navigate('Home')}>
+                            <Icon
+                                name="home"
+                                size={30}
+                                color={focused ? '#e32f45' : '#748c94'}
+                            />
+                            <Text
+                                style={{
+                                    color: focused ? '#000' : '#748c94',
+                                    fontSize: 16,
+                                }}>
+                                Home
+                            </Text>
+                        </TouchableOpacity>
+                    )
                 }}
             />
-            <Tab.Screen name="Account" component={MyAccount}
+            <Tab.Screen
+                name="Account"
+                component={MyAccount}
                 options={{
-                    title: 'MyAccount',
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
+                    tabBarIcon: ({ focused }) => (
+                        <TouchableOpacity
+                            style={{ alignItems: 'center', justifyContent: 'center' }}
+                            onPress={() => navigation.navigate('Account')}>
+                            <Icon
+                                name="manage-accounts"
+                                size={30}
+                                color={focused ? '#e32f45' : '#748c94'}
+                            />
+                            <Text
+                                style={{
+                                    color: focused ? '#000' : '#748c94',
+                                    fontSize: 16,
+                                }}>
+                                Account
+                            </Text>
+                        </TouchableOpacity>
+                    )
                 }}
             />
-            <Tab.Screen name="Register" component={Register}
+            <Tab.Screen
+                name="Register"
+                component={Register}
                 options={{
-                    title: 'Register',
-                    headerStyle: {
-                        backgroundColor: '#f4511e',
-                    },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
+                    tabBarIcon: ({ focused }) => (
+                        <TouchableOpacity
+                            style={{ alignItems: 'center', justifyContent: 'center' }}
+                            onPress={() => navigation.navigate('Register')}>
+                            <Icon
+                                name="app-registration"
+                                size={30}
+                                color={focused ? '#e32f45' : '#748c94'}
+                            />
+                            <Text
+                                style={{
+                                    color: focused ? '#000' : '#748c94',
+                                    fontSize: 16,
+                                }}>
+                                Register
+                            </Text>
+                        </TouchableOpacity>
+                    )
                 }}
             />
         </Tab.Navigator>
