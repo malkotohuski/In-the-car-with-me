bfc4f8f0d65dcc3f774fcd2a4fa3a7c42e330125
Daniel Dimitrov <malkotohuski@gmail.com>
2023-12-12 17:07:16 +0200

    new screen add

---
+++ screens/Category/Confirm.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+
+function Confirm({ route, navigation }) {
+    const {
+        selectedVehicle,
+        markedSeats,
+        registrationNumber,
+    } = route.params;
+
+    return (
+
+     )
+}
+
+export default Confirm;
