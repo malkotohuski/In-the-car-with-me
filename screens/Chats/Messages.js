a6e8695be9342c26403dc53c0a7b44d9bfc23191
Daniel Dimitrov <malkotohuski@gmail.com>
2024-02-29 18:17:19 +0200

    add freind reqests

--- screens/Chats/Messages.js
+++ screens/Chats/Messages.js
@@ -1,37 +1,59 @@
 import React from "react";
-import { Pressable, StyleSheet, Text, Image, View } from 'react-native';
+import { Pressable, StyleSheet, Text, Image, View, SafeAreaView, TouchableOpacity } from 'react-native';
 import { useAuth } from "../Authentication/AuthContext";
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-
-const Messages = () => {
+const Messages = ({ navigation }) => {
     const { user } = useAuth();
     console.log("ssss", user);
     return (
-        <Pressable style={{ flexDirection: "row", alignItems: "center", marginVertical: 10 }}>
-            <View>
-                <Image
-                    style={{
-                        width: 50,
-                        height: 50,
-                        borderRadius: 25,
-                        resizeMode: 'cover',
-                    }}
-                    source={{ uri: user?.user?.userImage }}
-                />
-            </View>
+        <SafeAreaView style={styles.mainContainer}>
+            <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                <View style={styles.header} >
+                    <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
+                        Messages
+                    </Text>
+                    <View style={{ width: 60 }} />
+                    <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+                        {/* Кастомизирайте бутона за връщане според вашите изисквания */}
+                        <Icons name="keyboard-backspace" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <Pressable style={{ flexDirection: "row", alignItems: "center", marginVertical: 10 }}>
+                    <View>
+                        <Image
+                            style={{
+                                width: 50,
+                                height: 50,
+                                borderRadius: 25,
+                                resizeMode: 'cover',
+                            }}
+                            source={{ uri: user?.user?.userImage }}
+                        />
+                    </View>
 
-            <View style={{ paddingLeft: 10 }}>
-                <Text>{user?.user?.username}</Text>
-                <Text>{user?.user?.fName} {user?.user?.lName}</Text>
+                    <View style={{ paddingLeft: 10 }}>
+                        <Text>{user?.user?.username}</Text>
+                        <Text>{user?.user?.fName} {user?.user?.lName}</Text>
+                    </View>
+                </Pressable>
             </View>
-        </Pressable>
+        </SafeAreaView>
     )
-
-
 }
 
 export default Messages;
 
 const styles = StyleSheet.create({
-
+    mainContainer: {
+        flex: 1,
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+        width: '100%',
+        padding: 16,
+        backgroundColor: '#f4511e',
+    },
 })
