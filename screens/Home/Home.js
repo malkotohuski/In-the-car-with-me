1d94a4d35ca6f5a38b50e1b05752bca8ad9d4d80
Daniel Dimitrov <malkotohuski@gmail.com>
2024-06-11 21:19:31 +0300

    features-find-bug-in-darkMode

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,13 +1,20 @@
-import React, { useContext } from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import i18n from './i18n';
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import styles from './styles';
 import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { DarkModeContext } from '../Authentication/DarkModeContext';
 
 function HomePage({ navigation }) {
     const { t } = useTranslation();
-    const { darkMode, backgroundImage } = useContext(DarkModeContext);
+    const [isBulgaria, setisBulgaria] = useState(false);
+
+    const changeLanguage = async (lng) => {
+        await i18next.changeLanguage(lng);
+        setisBulgaria(lng === 'bg');
+    };
 
     const handlerVehicle = () => {
         navigation.navigate('Vehicle');
@@ -33,20 +40,10 @@ function HomePage({ navigation }) {
         navigation.navigate('Notifications');
     }
 
-    const getTextStyle = () => {
-        return {
-            color: darkMode ? '#fff' : '#000',
-        };
-    };
-
-    const getIconColor = () => {
-        return darkMode ? '#fff' : '#000';
-    };
-
     return (
         <View style={styles.homepage}>
             <Image
-                source={backgroundImage}
+                source={require('../../images/home2-background.jpg')}
                 style={styles.backgroundImage}
             />
             <View style={{ flex: 1, justifyContent: 'center' }}>
@@ -59,7 +56,9 @@ function HomePage({ navigation }) {
                             source={require('../../images/eng1-flag.png')}
                             style={styles.flagImage}
                         />
-                        <Text style={styles.languageText}>{t('English')}</Text>
+                        <Text
+                            style={styles.languageText}
+                        >{t('English')}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity
                         style={styles.languageButton}
@@ -69,45 +68,56 @@ function HomePage({ navigation }) {
                             source={require('../../images/bulg-flag.png')}
                             style={styles.flagImage}
                         />
-                        <Text style={styles.languageText}>{t('Bulgarian')}</Text>
+                        <Text
+                            style={styles.languageText}
+                        >{t('Bulgarian')}</Text>
                     </TouchableOpacity>
                 </View>
 
                 <View style={styles.menuImages}>
                     <View>
-                        <TouchableOpacity style={styles.vehicleButton} onPress={handlerVehicle}>
-                            <Text style={[styles.textButtons, getTextStyle()]}>{t('Create a route')}</Text>
+                        <TouchableOpacity style={styles.vehicleButton} onPress={handlerVehicle} >
+                            <Text
+                                style={styles.textButtons}
+                            >{t('Create a route')}</Text>
                         </TouchableOpacity>
                     </View>
                     <View>
-                        <TouchableOpacity style={styles.routeRequestButton} onPress={handlerRouteRequest}>
-                            <Text style={[styles.textButtons, getTextStyle()]}>{t('Route request')}</Text>
+                        <TouchableOpacity style={styles.routeRequestButton} onPress={handlerRouteRequest} >
+                            <Text
+                                style={styles.textButtons}
+                            >{t('Route request')}</Text>
                         </TouchableOpacity>
                     </View>
                     <View>
-                        <TouchableOpacity style={styles.routeViewerButton} onPress={handlerRouteViewer}>
-                            <Text style={[styles.textButtons, getTextStyle()]}>{t('Routes history')}</Text>
+                        <TouchableOpacity style={styles.routeViewerButton} onPress={handlerRouteViewer} >
+                            <Text
+                                style={styles.textButtons}
+                            >{t('Routes history')}</Text>
                         </TouchableOpacity>
                     </View>
                     <View>
-                        <TouchableOpacity style={styles.reportingButton} onPress={handlerReporting}>
-                            <Text style={[styles.textButtons, getTextStyle()]}>{t('Reporting')}</Text>
+                        <TouchableOpacity style={styles.reportingButton} onPress={handlerReporting} >
+                            <Text
+                                style={styles.textButtons}
+                            >{t('Reporting')}</Text>
                         </TouchableOpacity>
                     </View>
                 </View>
                 <View style={styles.centeredTextContainer}>
-                    <Text style={[styles.heading, getTextStyle()]}>{t('In the car with me')}</Text>
-                    <Text style={[styles.moto, getTextStyle()]}>{t('We travel freely')}</Text>
+                    {/* Your content goes here */}
+                    <Text style={styles.heading}>{t('In the car with me')}</Text>
+                    <Text style={styles.moto}>{t('We travel freely')}</Text>
                 </View>
                 <View style={styles.footer}>
                     <TouchableOpacity style={styles.footerIcon} onPress={handlerNotificationScreen}>
-                        <Icons name="routes" size={34} color={getIconColor()} />
+                        <Icons name="routes" size={34} color="#000000" />
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.footerIcon} onPress={handlerChatScreen}>
-                        <Icons name="chat" size={34} color={getIconColor()} />
+                        <Icons name="chat" size={34} color="#080808" />
                     </TouchableOpacity>
                     <TouchableOpacity style={styles.footerIcon} onPress={handlerNotificationScreen}>
-                        <Icons name="bell" size={34} color={getIconColor()} />
+                        <Icons name="bell" size={34} color="#000000" />
                     </TouchableOpacity>
                 </View>
             </View>
