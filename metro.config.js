41574d822d3bf25a8db2f55844c6f4d11ff075b7
Daniel Dimitrov <malkotohuski@gmail.com>
2024-08-01 22:03:32 +0300

    fix

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-const config = getDefaultConfig(__dirname);
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
 
-module.exports = config;
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
