5e00f05fb84685042ecdc0bcec98db3ea961fb60
Daniel Dimitrov <malkotohuski@gmail.com>
2024-10-16 22:15:37 +0300

    Prepare app for Heroku deployment

--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -228,7 +228,7 @@ export default StyleSheet.create({
         fontSize: 34,
         marginBottom: 30,
         fontWeight: 'bold',
-        color: 'white'
+        color: '#010101'
     },
     profilePictureContainer: {
         alignItems: 'center',
