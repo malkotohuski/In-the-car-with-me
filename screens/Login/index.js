e64a6d1711b220f46371dde2c2a51a416b35b344
Daniel Dimitrov <malkotohuski@gmail.com>
2024-12-02 18:57:10 +0200

    little styles change

--- screens/Login/index.js
+++ screens/Login/index.js
@@ -78,7 +78,7 @@ export default function Login({ navigation, route }) {
                                 source={require('../../images/loading_image.png')}
                                 style={styles.backgroundImage}
                             />
-                            <Animated.Text style={{ fontSize: 26, color: '#010101', fontWeight: 'bold', position: 'absolute', top: '50%', alignSelf: 'center', opacity }}>
+                            <Animated.Text style={{ fontSize: 30, color: '#010101', fontWeight: 'bold', position: 'absolute', top: '50%', alignSelf: 'center', opacity }}>
                                 {t('Loading...')}
                             </Animated.Text>
                         </>
